78204c039409ed1261843c5478803a47f875581b
lemejiamo <luismejia69@gmail.com>
2021-07-21 21:31:32 -0500

    goes through a binary tree using pre-order traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ *  binary_tree_delete - deletes an entire binary tree
+ *  @tree:  pointer to a parent node
+ *  Return - none
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func != NULL)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
