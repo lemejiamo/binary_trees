77e95b8d0df12f5cc7ce2d6b92458b1782d7d574
lemejiamo <luismejia69@gmail.com>
2021-07-21 23:00:51 -0500

    binary tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,16 +6,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t height = 0;
+    size_t right_height = 0, left_height = 0;
 
     if (tree == NULL)
         return (0);
 
     if (tree->left)
-        height += 1;
+        left_height = 1 + binary_tree_height(tree->left);
 
     if (tree->right)
-        height += 1;
+        right_height = 1 + binary_tree_height(tree->right);
     
-    return (height);
+    if (left_height > right_height)
+        return (left_height);
+
+    return (right_height);
 }
