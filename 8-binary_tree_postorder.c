646a1f07a7527c657c4862b52475cfe8deb87f75
lemejiamo <luismejia69@gmail.com>
2021-07-21 22:37:27 -0500

    binary tree orders

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -4,12 +4,12 @@
  *  @tree:  pointer to a parent node
  *  Return - none
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if ((tree) == NULL || (func) == NULL)
 		return;
 
-	binary_tree_inorder(tree->left, func);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
-	binary_tree_inorder(tree->right, func);
 }
