32592df186ce9455d5681e69afc8088a7526173b
lemejiamo <luismejia69@gmail.com>
2021-07-21 22:52:14 -0500

    binary tree height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ *  binary_tree_height - deletes an entire binary tree
+ *  @tree:  pointer to a parent node
+ *  Return - none
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t height = 0;
+
+    if (tree->left)
+        height += 1;
+
+    if (tree->right)
+        height += 1;
+    
+    return (height);
+}
