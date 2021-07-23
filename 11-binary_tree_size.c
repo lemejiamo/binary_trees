afad7871b4e30446b5118797dd6c4256adcbb85d
jiuder <julioarenas85@gmail.com>
2021-07-22 22:28:25 -0500

    [Style ~ 11-binary](Style Correction)

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,44 +1,49 @@
 #include "binary_trees.h"
-
-
-
+/**
+ * tree_size - measure the size of the tree.
+ * @tree: pointer to the node.
+ * @level:node level in the tree.
+ * @deep:tree size control.
+ * Return: NULL.
+ */
 size_t tree_size(const binary_tree_t *tree, size_t *level, size_t *deep)
 {
-    if (tree->right != NULL)
-    {
-        if (*level >= *deep)
-            *deep += 1;
-         *level += 1;
-        tree_size(tree->right, level, deep);
-    }
+	if (tree->right != NULL)
+	{
+		if (*level >= *deep)
+			*deep += 1;
+		*level += 1;
+		tree_size(tree->right, level, deep);
+	}
 
-    if (tree->left != NULL)
-    {
-        if (*level >= *deep)
-            *deep += 1;
-         *level += 1;
-        tree_size(tree->left, level, deep);
-    }
+	if (tree->left != NULL)
+	{
+		if (*level >= *deep)
+			*deep += 1;
+		*level += 1;
+		tree_size(tree->left, level, deep);
+	}
 
-    if (tree->left == NULL && tree->right == NULL)
-        return ((*level - 1));
-    
-    return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return ((*level - 1));
+
+	return (0);
 
 }
 
 /**
- *  binary_tree_height - deletes an entire binary tree
- *  @tree:  pointer to a parent node
- *  Return - none
+ *  binary_tree_size - size tree.
+ *  @tree:  pointer to a parent node.
+ *  Return: tree size.
  */
- size_t binary_tree_size(const binary_tree_t *tree)
- {
-    size_t *deep = NULL, *level = NULL, deep_t = 1, level_t = 1;
-    deep = &deep_t;
-    level = &level_t;
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t *deep = NULL, *level = NULL, deep_t = 1, level_t = 1;
 
-    tree_size(tree, level, deep);
+	deep = &deep_t;
+	level = &level_t;
 
-    return (deep_t);
- }
+	tree_size(tree, level, deep);
+
+	return (deep_t);
+}
