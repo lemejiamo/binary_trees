4fdb0f8a50e9697fb8d60af9c7a6ccafead0c03a
jiuder <julioarenas85@gmail.com>
2021-07-22 20:35:44 -0500

    [Style ~ 4-binary](Style Correction)

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,17 +1,17 @@
 #include "binary_trees.h"
 /**
- *  binary_tree_delete - deletes an entire binary tree
- *  @tree:  pointer to a parent node
- *  Return - none
+ *   binary_tree_is_leaf - checks if a node is a leaf
+ *  @node:  pointer to a parent node
+ *  Return: NULL
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
 
-    if (node->left == NULL && node->right == NULL)
-        return (1);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
 
-    return (0);
+	return (0);
 
 }
