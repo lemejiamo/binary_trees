f0a2dd78bf3b73c7ee7cee055ad0239e4065b388
jiuder <julioarenas85@gmail.com>
2021-07-22 21:40:40 -0500

    [Style ~ 9-binary](Style Correction)

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,24 +1,24 @@
 #include "binary_trees.h"
 /**
  *  binary_tree_height - deletes an entire binary tree
- *  @tree:  pointer to a parent node
- *  Return - none
+ *  @tree: pointer to a parent node
+ *  Return: rigth
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t right_height = 0, left_height = 0;
+	size_t right_height = 0, left_height = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left)
-        left_height = 1 + binary_tree_height(tree->left);
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
 
-    if (tree->right)
-        right_height = 1 + binary_tree_height(tree->right);
-    
-    if (left_height > right_height)
-        return (left_height);
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
 
-    return (right_height);
+	if (left_height > right_height)
+		return (left_height);
+
+	return (right_height);
 }
